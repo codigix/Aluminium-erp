9e724fc3361b5032e110afffea4a0046d9e7bf91
reactjscodigix <reactjscodigix@gmail.com>
2026-01-03 12:36:30 +0530

    done

--- backend/src/routes/company.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getCompanyInfo } from '../controllers/CompanyController.js'
-
-const router = express.Router()
-
-router.get('/', getCompanyInfo)
-
-export default router
--- frontend/src/components/Selling/index.js
+++
@@ -1,8 +0,0 @@
-// Selling Module Modal Components
-export { default as CreateQuotationModal } from './CreateQuotationModal'
-export { default as CreateSalesOrderModal } from './CreateSalesOrderModal'
-export { default as ViewSalesOrderModal } from './ViewSalesOrderModal'
-export { default as EditSalesOrderModal } from './EditSalesOrderModal'
-export { default as CreateDeliveryNoteModal } from './CreateDeliveryNoteModal'
-export { default as CreateInvoiceModal } from './CreateInvoiceModal'
-export { default as CreateCustomerModal } from './CreateCustomerModal'
