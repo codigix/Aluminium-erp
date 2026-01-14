ba6a8494c5501e5df5a61eaf902528e72bff02cf
codigix <codigixinfotech@gmail.com>
2026-01-14 10:37:45 +0530

    push

--- backend/seed.js
+++ backend/seed.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'backend',
+  password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'sales_erp',
   waitForConnections: true,
   connectionLimit: 10,
